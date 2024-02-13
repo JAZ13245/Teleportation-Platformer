f66b15bf0f6676e0997d7655a097fc10f6d917d6
James Zilberman <jaz1324576@gmail.com>
2024-02-13 03:45:31 -0500

    Level and Mirror Edits

--- Teleportation Platformer/Assets/Scripts/Mirror.cs
+++ Teleportation Platformer/Assets/Scripts/Mirror.cs
@@ -6,6 +6,8 @@ public class Mirror : MonoBehaviour
 {
     [SerializeField]
     private MirrorTrigger trigger;
+    [SerializeField] float exitVelocityIncrease = 1.5f;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Arrow arrow = collision.gameObject.GetComponent<Arrow>();
@@ -22,6 +24,6 @@ public class Mirror : MonoBehaviour
         Debug.DrawLine(contact.point, contact.point + outgoing, Color.red, 10f);
 
         arrow.gameObject.transform.position = contact.point + (0.1f *outgoing);
-        rb.velocity = outgoing;
+        rb.velocity = outgoing  * exitVelocityIncrease;
     }
 }
